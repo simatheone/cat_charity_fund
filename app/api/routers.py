d4c9301478287dd6844b210f0c39038fb4ff4955
simatheone <samone07@mail.ru>
2022-09-15 19:57:50 +0300

    Include charityproject router, donation router in main_router

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,9 +1,19 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import (
-    user_router
+    user_router, donation_router, charityproject_router
 )
 
 main_router = APIRouter()
 
 main_router.include_router(user_router)
+
+main_router.include_router(
+    donation_router, prefix='/donation', tags=['Donations']
+)
+
+main_router.include_router(
+    charityproject_router,
+    prefix='/charity_project',
+    tags=['Charity Projects']
+)
