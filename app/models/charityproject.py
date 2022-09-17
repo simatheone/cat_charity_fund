08b10ccfb15738ac537e591e97e26683c292afea
simatheone <samone07@mail.ru>
2022-09-17 17:55:26 +0300

    Rename charityproject to charity_project

--- app/models/charityproject.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, String, Text
-
-from app.core.db import PreBaseDonationCharity
-
-
-class CharityProject(PreBaseDonationCharity):
-    name = Column(String(100), unique=True, nullable=False)
-    description = Column(Text, nullable=False)
