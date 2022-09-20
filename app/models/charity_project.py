b9c115f8402b72ddb3286d7455cc0f9732eeb0a1
simatheone <samone07@mail.ru>
2022-09-20 21:12:32 +0300

    Add __repr__ method for CharityProject model

--- app/models/charity_project.py
+++ app/models/charity_project.py
@@ -6,3 +6,6 @@ from app.core.db import PreBaseDonationCharity
 class CharityProject(PreBaseDonationCharity):
     name = Column(String(100), unique=True, nullable=False)
     description = Column(Text, nullable=False)
+
+    def __repr__(self) -> str:
+        return f'Фонд {self.name}'
