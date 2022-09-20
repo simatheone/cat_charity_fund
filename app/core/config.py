2d24635de98d0501ed49e6755b3b5198b69d4009
simatheone <samone07@mail.ru>
2022-09-20 21:10:35 +0300

    Change DB name

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     app_title: str = 'Charity Project'
     app_description: str = 'Приложение для Благотворительного фонда поддержки котиков QRKot.'
-    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
+    database_url: str = 'sqlite+aiosqlite:///./charity_fund.db'
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
